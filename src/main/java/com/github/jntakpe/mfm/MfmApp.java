c814c523663ef85d9f96ed4f58ca2cee26298143
jntakpe <j.ntakpe@gmail.com>
2015-03-01 20:00:36 +0100

    Début conf backend Spring Boot

---
+++ src/main/java/com/github/jntakpe/mfm/MfmApp.java
@@ -0,0 +1,55 @@
+package com.github.jntakpe.mfm;
+
+import com.github.jntakpe.mfm.config.Constants;
+import org.apache.commons.lang3.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Classe démarrant l'application Spring Boot
+ *
+ * @author jntakpe
+ */
+@Configuration
+@EnableConfigurationProperties
+@ComponentScan("com.github.jntakpe.mfm")
+@EnableAutoConfiguration(exclude = {MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
+public class MfmApp extends SpringBootServletInitializer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MfmApp.class);
+
+    /**
+     * Démarre l'application en mode 'embedded'
+     *
+     * @param args arguments passés par le goal maven
+     */
+    public static void main(String[] args) {
+        LOG.info("Starting app with embedded Tomcat");
+        new SpringApplication(MfmApp.class).run(args);
+    }
+
+    /**
+     * Démarrage sur un serveur classique (Tomcat, Jetty, JBoss, etc ...)
+     *
+     * @param application builder de la configuration Spring Boot
+     * @return la configuration prête à être lancée
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        LOG.info("Starting app on external Tomcat");
+        String profile = SystemUtils.IS_OS_LINUX ? Constants.PROD_PROFILE : Constants.DEV_PROFILE;
+        LOG.info("Profile {} selected", profile);
+        application.profiles(profile);
+        return application.sources(MfmApp.class);
+    }
+
+}
---
+++ src/main/java/com/github/jntakpe/mfm/config/Constants.java
@@ -0,0 +1,21 @@
+package com.github.jntakpe.mfm.config;
+
+/**
+ * Constantes de l'application
+ *
+ * @author jntakpe
+ */
+public class Constants {
+
+    public static final String DEV_PROFILE = "dev";
+
+    public static final String PROD_PROFILE = "prod";
+
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    public static final String USER = "ROLE_USER";
+
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    public static final String API_URI = "/api";
+}
