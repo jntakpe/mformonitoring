00c5da2e240115a29dcd3fb49ff9436e07ed675d
jntakpe <j.ntakpe@gmail.com>
2015-03-02 15:49:40 +0100

    Création du repository pour l'entité Application

---
+++ src/main/java/com/github/jntakpe/mfm/repository/ApplicationRepository.java
@@ -0,0 +1,12 @@
+package com.github.jntakpe.mfm.repository;
+
+import com.github.jntakpe.mfm.domain.Application;
+
+/**
+ * Publication des méthodes de gestion de l'entité {@link com.github.jntakpe.mfm.domain.Application}
+ *
+ * @author jntakpe
+ */
+public interface ApplicationRepository extends GenericRepository<Application> {
+
+}
