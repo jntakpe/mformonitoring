eabf8e3bf94e11e6291cdf347ca31fa7332cc9d3
jntakpe <j.ntakpe@gmail.com>
2015-03-02 15:50:40 +0100

    Création de la classe de service pour l'entité Application

---
+++ src/main/java/com/github/jntakpe/mfm/service/ApplicationService.java
@@ -0,0 +1,13 @@
+package com.github.jntakpe.mfm.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Services associés à l'entité {@link com.github.jntakpe.mfm.domain.Application}
+ *
+ * @author jntakpe
+ */
+@Service
+public class ApplicationService {
+
+}
