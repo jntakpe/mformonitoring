3bfe8783eae7ee6c5804118ec77bba7bcfc53c51
jntakpe <j.ntakpe@gmail.com>
2015-03-02 13:50:00 +0100

    Ajout énumération des environnements

---
+++ src/main/java/com/github/jntakpe/mfm/domain/Environnements.java
@@ -0,0 +1,18 @@
+package com.github.jntakpe.mfm.domain;
+
+/**
+ * Enumérations des environnements
+ *
+ * @author jntakpe
+ */
+public enum Environnements {
+
+    DEVELOPPEMENT,
+    ASSEMBLAGE,
+    INTEGRATION,
+    QUALIFICATION,
+    RECETTE,
+    PRE_PRODUCTION,
+    PRODUCTION
+
+}
