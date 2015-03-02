25a0db9a23b29341544b2ac7b5a7688b9fc3c6ea
jntakpe <j.ntakpe@gmail.com>
2015-03-02 15:48:28 +0100

    Création repo générique

---
+++ src/main/java/com/github/jntakpe/mfm/repository/GenericRepository.java
@@ -0,0 +1,14 @@
+package com.github.jntakpe.mfm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+/**
+ * Interface publiant les méthodes génériques de gestion des entités persistés via Spring Data JPA
+ *
+ * @author jntakpe
+ */
+@NoRepositoryBean
+public interface GenericRepository<T> extends JpaRepository<T, Long> {
+
+}
