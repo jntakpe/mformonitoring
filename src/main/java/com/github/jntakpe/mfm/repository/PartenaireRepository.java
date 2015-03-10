f5489c0a6aedc3ec03f78ed66e79e3187c812d49
jntakpe <j.ntakpe@gmail.com>
2015-03-10 22:49:37 +0100

    Ajout du repository Partenaire

---
+++ src/main/java/com/github/jntakpe/mfm/repository/PartenaireRepository.java
@@ -0,0 +1,12 @@
+package com.github.jntakpe.mfm.repository;
+
+import com.github.jntakpe.mfm.domain.Partenaire;
+
+/**
+ * Publication des méthodes de gestion de l'entité {@link Partenaire}
+ *
+ * @author jntakpe
+ */
+public interface PartenaireRepository extends GenericRepository<Partenaire> {
+
+}
