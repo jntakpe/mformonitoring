00d1636dfde9dca871205de3ce60143912167913
jntakpe <j.ntakpe@gmail.com>
2015-03-31 23:00:05 +0200

    Uniformisation affichage statut

--- src/main/webapp/static/js/applications/health/health.controller.js
+++ src/main/webapp/static/js/applications/health/health.controller.js
@@ -1,8 +1,9 @@
-mfmApp.controller('HealthController', function (PartnerService, application) {
+mfmApp.controller('HealthController', function (ApplicationService, PartnerService, application) {
     "use strict";
 
     var vm = this;
     PartnerService.find(application.id).then(function (response) {
         vm.data = response.data;
     });
+    vm.icon = ApplicationService.icon;
 });
